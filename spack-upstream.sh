1378f1e538305fcd5fc097ff9fe80a106599b4ad
Daniel Trahan <mtrahan@gust01.hsn.gu.hpc.ucar.edu>
2023-03-27 13:24:27 -0600

    Replaced official spack link with NCARs branch.

--- spack-upstream.sh
+++ spack-upstream.sh
@@ -6,8 +6,8 @@ print_usage () {
     echo "spack-upstream-setup.sh: Script to set up a local spack instance and link it to the upstream system spack"
     echo "usage:"
     echo "       -v | --verbose                  : print out each installation step to the terminal"
-    echo "            --prefix                   : specify spack installation location. Default: /glade/work/$USER/spack_version"
-    echo "            --version=<install-path>   : install a specific version of spack (default: latest)"
+    echo "            --prefix=<install-path>    : specify spack installation location. Default: /glade/work/$USER/spack_<version>"
+    # echo "            --version=<install-path>   : install a specific version of spack (default: latest)"
     echo "            --modify-rc=<True|False>   : modify your startup script to automatically load Spack at startup"
     # echo "            --modules"
     # echo "            --sync"
@@ -61,24 +61,24 @@ modify_rc () {
 while [ "$#" -gt 0 ]
 do
     case "${1}" in
-        (--prefix?*)     PREFIX="${2}"; shift ;;
-        (-v | --verbose) VERBOSE="True" ;;
-        (-h | --help)    print_usage; exit 0 ;;
-        (--modify-rc?*)  MODIFY_RC="${2}"; shift ;;
-        (--version?*)    VERSION="${2}"; shift ;;
-        (*)              echo "spack-upstream error: unrecognized option ${1}"; print_usage; exit 1 ;;
+	(--prefix=?*)     PREFIX="$(echo ${1} | cut -f 2 -d '=')" ;;
+        (-v | --verbose)  VERBOSE="True" ;;
+	(-h | --help)     print_usage; exit 0 ;;
+	(--modify-rc=?*)  MODIFY_RC="$(echo ${1} | cut -f 2 -d '=')" ;;
+        # (--version=?*)    VERSION="$(echo ${1} | cut -f 2 -d '=')" ;;
+        (*)               echo "spack-upstream error: unrecognized option ${1}"; print_usage; exit 1 ;;
     esac
     shift
 done
 
 # Temporary Version Identifier
-if [ -z $VERSION ]; then
+if [ -z "$VERSION" ]; then
     VERSION=$(echo "$(/glade/u/apps/gust/default/spack/bin/spack --version | grep -o '[0-9]*\.[0-9]*\.[0-9]*')") 
 fi
 echo "Spack version: $VERSION"
 
 # Set install path
-if [ -z $PREFIX ]; then
+if [ -z "$PREFIX" ]; then
     INSTALL_DIR="/glade/work/$USER/spack-$VERSION"
 else
     INSTALL_DIR=$PREFIX
@@ -86,25 +86,30 @@ fi
 echo "Prefix: $INSTALL_DIR"
 
 # Clone repo, copy yaml, and modify rc
-log "git clone https://github.com/spack/spack.git --branch v$VERSION $INSTALL_DIR"
-git clone https://github.com/spack/spack.git --branch v$VERSION $INSTALL_DIR
+
+#log "git clone https://github.com/spack/spack.git --branch v$VERSION $INSTALL_DIR"
+#git clone https://github.com/spack/spack.git --branch v$VERSION $INSTALL_DIR
+
+log "git clone https://github.com/NCAR/csg-spack-fork $INSTALL_DIR"
+git clone https://github.com/NCAR/csg-spack-fork $INSTALL_DIR
+
 copy_yaml 
 
 echo "Spack has been successfully installed and configure for upstream use in: $INSTALL_DIR"
 
-if [ "MODIFY_RC" = "True" ]; then
+if [ "$MODIFY_RC" = "True" ]; then
     modify_rc
-elif [ "MODIFY_RC" = "False" ]; then
+elif [ "$MODIFY_RC" = "False" ]; then
     exit 0
 else
-    echo "Would you like this installer to automatically initialize spack on log in by modifying .bashrc?"
+    echo "Would you like this installer to automatically initialize spack at login by modifying .bashrc?"
     echo "(y|n)"
     while true; do
         read MOD
-        if [ $MOD == 'y' ]; then
+        if [ "$MOD" == 'y' ]; then
             modify_rc
             exit 0
-        elif [ $MOD == 'n' ]; then
+        elif [ "$MOD" == 'n' ]; then
             echo "To initialize your shell for spack use please run the command:"
             echo ". $INSTALL_DIR/share/spack/setup-env.sh"
             exit 0
